de90e2a6f1a7e78054a1e43ec1c803afda922cad
fadillf <fadillahfadlurrahman@gmail.com>
2020-08-04 20:08:53 +0700

    Latihan LinearLayout & RelativeLayout 2

---
+++ app/src/main/java/com/fdl/pwpb/DaftarActivity2.java
@@ -0,0 +1,29 @@
+package com.fdl.pwpb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DaftarActivity2 extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daftar2);
+    }
+
+    @Override
+    public void findViews() {
+
+    }
+
+    @Override
+    public void initViews() {
+
+    }
+
+    @Override
+    public void initListeners() {
+
+    }
+}

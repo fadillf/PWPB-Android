52b82980a98a0d34778126545fd7f8f04c56b0c5
fadillf <fadillahfadlurrahman@gmail.com>
2020-08-04 20:11:03 +0700

    Edit Structure

--- app/src/main/java/com/fdl/pwpb/MainActivity.java
+++ app/src/main/java/com/fdl/pwpb/MainActivity.java
@@ -1,14 +1,28 @@
 package com.fdl.pwpb;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void findViews() {
+        
+    }
+
+    @Override
+    public void initViews() {
+
+    }
+
+    @Override
+    public void initListeners() {
+
     }
 }

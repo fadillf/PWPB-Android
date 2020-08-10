9eefe5f911d1acc7ba3f7cccbe646f410d2d7259
fadillf <fadillahfadlurrahman@gmail.com>
2020-08-10 20:23:00 +0700

    Tugas Table Layout

---
+++ app/src/main/java/com/fdl/pwpb/FrameLayoutActivity2.java
@@ -0,0 +1,15 @@
+package com.fdl.pwpb;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FrameLayoutActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame_layout_2);
+    }
+
+}
